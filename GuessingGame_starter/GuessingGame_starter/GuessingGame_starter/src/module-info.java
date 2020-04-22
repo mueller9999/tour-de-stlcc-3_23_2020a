2d8f22339c1033c242bec0d81701784be73c7a12
mueller9999 <36347726+mueller9999@users.noreply.github.com>
2020-04-22 11:13:56 -0500

    STLCC VITUAL TOUR
    took out  the slot machine

---
+++ GuessingGame_starter/GuessingGame_starter/GuessingGame_starter/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author miken
+ *
+ */
+module slotMachineEclipse {exports guessinggame;exports business;exports servlets;requires GuessingGame;requires antlr;requires appframework;requires hibernate.core;requires hibernate.jpa;requires java.desktop;requires java.naming;requires java.sql;requires javax.persistence;requires javax.servlet.3.0.0.v201112011016;requires rt;}
